9acd7161b2e38159ad4ac65c26b60d0f5609b002
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2025-03-01 10:06:14 +0200

    Update dashboard actions

--- src/state/actions/dashboardActions.ts
+++ src/state/actions/dashboardActions.ts
@@ -1,21 +1,29 @@
 import actionConstants from '../constants/dashboardConstants';
 
 export async function createSimulation(simulation: any) {
+  // @todo: modify when receiving
   // const argumentsArray = Object.keys(argumentsObject).map(((entry) => argumentsObject[entry]));
   // const config = { command, argumentsArray };
+
   const { data }: any = await fetch('/api/spawn', { method: 'POST', body: JSON.stringify(simulation) });
 
   return { type: actionConstants.CREATE_SIMULATION, data };
 }
 
-export function readAllSimulations() {
-  return null;
+export async function readAllSimulations() {
+  const { data }: any = await fetch('/api/spawn', { method: 'GET' });
+
+  return { type: actionConstants.READ_ALL_SIMULATIONS, data };
 }
 
-export function updateSimulation() {
-  return null;
+export async function updateSimulation(simulation: any) {
+  const { data }: any = await fetch('/api/spawn', { method: 'PUT', body: JSON.stringify(simulation) });
+
+  return { type: actionConstants.UPDATE_SIMULATION, data };
 }
 
-export function deleteSimulation() {
-  return null;
+export async function deleteSimulation(simulation: any) {
+  const { data }: any = await fetch('/api/spawn', { method: 'DELETE', body: JSON.stringify(simulation) });
+
+  return { type: actionConstants.DELETE_SIMULATION, data };
 }
