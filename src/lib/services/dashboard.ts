b8d942472f8842d52d5b4b66e65eb31651bf12ab
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2025-03-14 17:46:41 +0200

    Type Dashboard related code

--- src/lib/services/dashboard.ts
+++ src/lib/services/dashboard.ts
@@ -3,7 +3,7 @@ import { Simulation } from '@/types/dashboard';
 
 export async function createSimulation(buildState: BashScript) {
   const response: Response = await fetch('/api/dashboard', { method: 'POST', body: JSON.stringify(buildState) });
-  const simulation: any = await response.json();
+  const simulation: Simulation = await response.json();
 
   return simulation;
 }
